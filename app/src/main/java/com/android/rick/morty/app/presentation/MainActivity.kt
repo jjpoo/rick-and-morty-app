ffb61f134c79ede322c30d2459f8d8e62f76a074
polina_nosova <polina.nosova@cantire.com>
2025-09-27 09:52:37 +0300

    fix: fixed pipelines: error: resource xml/data_extraction_rules (aka com.android.rick.morty.app:xml/data_extraction_rules) not found.

--- app/src/main/java/com/android/rick/morty/app/presentation/MainActivity.kt
+++ app/src/main/java/com/android/rick/morty/app/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.android.rick.morty.app
+package com.android.rick.morty.app.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.android.rick.morty.app.ui.theme.RickandmortyappTheme
+import com.android.rick.morty.app.presentation.ui.theme.RickandmortyappTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
